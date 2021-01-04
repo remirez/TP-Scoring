c441f7752024ba85b6b93dcb3945969eb3374bbd
Remy McConnell <remy.m@floversal.com>
2021-01-03 20:06:27 -0600

    update

--- pimaindiansdiabetes.R
+++ pimaindiansdiabetes.R
@@ -1,5 +1,20 @@
 library(ggplot2)
 PimaIndiansDiabetes <- read.table("PimaIndiansDiabetes.csv", sep = ";", header=TRUE)
-ggplot(data=PimaIndiansDiabetes, aes(x=age, y=diabetes)) +
-  geom_bar(stat="identity")
-plot
+
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$insulin) # 0.130548
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$pregnant) # 0.2218982
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$pressure) # 0.06506836
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$glucose) # 0.4665814
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$triceps) # 0.07475223
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$mass) # 0.2926947
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$pedigree) # 0.1738441
+cor(PimaIndiansDiabetes$diabetes, PimaIndiansDiabetes$age) # 0.238356
+
+lin_reg <- lm(diabetes ~ ., data=PimaIndiansDiabetes)
+summary(lin_reg)$r.squared
+plot(lin_reg, which=1)
+plot(PimaIndiansDiabetes$diabetes, predict(lin_reg), xlab="observed", ylab="predicted")
+
+log_reg <- glm(diabetes ~ ., data=PimaIndiansDiabetes, family="binomial")
+coef(log_reg)
+summary(log_reg)
